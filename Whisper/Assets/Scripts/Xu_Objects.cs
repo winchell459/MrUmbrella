59f58fe8892a3d4845a63e2680584b40023805d8
john-xu-1 <63273190+john-xu-1@users.noreply.github.com>
2020-05-25 18:42:01 -0700

    Day1
    DId stuff

---
+++ Whisper/Assets/Scripts/Xu_Objects.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xu_Objects : MonoBehaviour
+{
+    void Start()
+    {
+        circle circle = new circle("circle", 3);
+        shape square = new square("square", 2, 3);
+
+        Debug.Log(circle.Area());
+
+
+    }
+
+  
+}
+
+public class shape
+{
+    public string name;
+    public int num_side;
+    
+}
+public class circle : shape
+{
+   float radius;
+   public circle(string name, float radius)
+   {
+        this.name = name;
+        num_side = 1;
+        this.radius = radius;
+   }
+   public float Area()
+    {
+        return Mathf.PI * radius * radius;
+    }
+}
+
+public class square : shape
+{
+    float length, height;
+    public square(string name, float height, float length)
+    {
+        this.name = name;
+        num_side = 1;
+        this.length = length;
+        this.height = height;
+    }
+    public float Area()
+    {
+        return length * height;
+    }
+}
