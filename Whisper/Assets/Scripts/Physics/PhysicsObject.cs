95c42297c7d9809d5d83fc86097d809efa469f07
winchell459 <johncmorris@gmail.com>
2020-06-22 13:53:14 -0700

    end of video 02

---
+++ Whisper/Assets/Scripts/Physics/PhysicsObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+
+        Movement(move);
+    }
+
+    void Movement(Vector2 move)
+    {
+
+        rb2d.position = rb2d.position + move;
+    }
+}
